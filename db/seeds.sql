8b93bde03cfbd11d8166bcd5787efc6729828cf2
Moetive <Moetive206@gmail.com>
2022-06-27 20:01:24 -0700

    Update
    Update

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments (department_namet)
+VALUES ("Operations"), ("Finance"), ("Sales");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Director", 80000, 1), ("Accountant", 75000, 2), ("Cashier", 35000, 3);
+
+INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+VALUES ("Johnny", "Doe" 1, NULL), ("Michael", "Rackson", 2, 1), ("Billy", "Jean", 3, 1);
